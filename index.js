2f2549e66629c790d9a04e9486db019fac65a2c9
Ashley <ashleyh5440@gmail.com>
2023-12-01 18:14:15 -0500

    downloaded npm and inquirer, added dist folder and pseudocode

--- index.js
+++ index.js
@@ -12,3 +12,6 @@
 // AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+//need to add the prompts and questions
+//need to write a svg file and save to dist folder
