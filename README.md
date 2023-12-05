1e69095b2c746645cdf017bbe2d71b6bcb590834
Ashley <ashleyh5440@gmail.com>
2023-12-05 14:03:23 -0500

    edited comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# svg-logo-maker
- Node.js command-line application that takes in user input to generate a logo and saves it as an SVG file
+# SVG logo generator
+   ![Github license](https://img.shields.io/badge/license-MIT-blue.svg) 
+  ## Description 
+  With this generator, you will be able to generate an SVG logo.
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+  Clone the repo from GitHub. Make sure to have Node.js installed with jest and inquier downloaded.
+  ## Usage
+  Quickly create a logo without hiring a designer.
+  ## License 
+ This project is licensed under the MIT license.
+  ## Contributing
+  [ashleyh5440](https://github.com/ashleyh5440)
+  ## Tests
+  Jest
+  ## Questions
+  If you have any questions, you can email me at ashleyh5440@gmail.com.
+  You can see more of my work at [ashleyh5440](https://github.com/ashleyh5440)
+  
+
--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
-//need to add the prompts and questions
-//need a function to create the file based on user answers
-//need to save file to dist folder
-//need 4 classes for shape, triangle, circle, and square
-//need tests for each class using Jest
-
 const inquirer = require("inquirer");
 const fs = require("fs");
 const {Circle, Triangle, Square} = require("./lib/shapes");
